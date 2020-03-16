096685c3a6ccb92109a679029bf9bd339287f8c3
Julius Carrell <carrell.jjm@gmail.com>
2020-03-16 21:10:05 +0000

    3

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,15 +4,15 @@ import "./Contact.css";
  class Contact extends Component {
      state = { 
          
-        nameavatar: "Jules",
+        name: "Jules",
+        avatar: "https://pbs.twimg.com/profile_images/1146769873374240770/e6Ia45_i_400x400.jpg",
         online: true ,
-    
     
     }
      render() { 
 
         return ( <div className={this.props.star?"Contact1":"Contact"}> 
-            <div className="avatar"> <img src= "https://pbs.twimg.com/profile_images/1146769873374240770/e6Ia45_i_400x400.jpg"></img> 
+            <div className="avatar"> <img src= {this.state.avatar}></img> 
             </div>
             <div className={this.props.star?"name1":"name"}>{this.state.nameavatar}</div>{" "}
             <div className={this.onlineOffline()}></div>
