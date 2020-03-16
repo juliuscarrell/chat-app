a57744d3753d90715d68eaa4a93e15e7791c466e
Julius Carrell <carrell.jjm@gmail.com>
2020-03-16 20:40:37 +0000

    3

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -12,7 +12,7 @@ import "./Contact.css";
      render() { 
 
         return ( <div className={this.props.star?"Contact1":"Contact"}> 
-            <div className="avatar"> <img src={this.props.image?"this.props.image":"https://www.drupal.org/files/issues/default-avatar.png"}></img> 
+            <div className="avatar"> <img src= "https://pbs.twimg.com/profile_images/1146769873374240770/e6Ia45_i_400x400.jpg"></img> 
             </div>
             <div className={this.props.star?"name1":"name"}>{this.state.nameavatar}</div>{" "}
             <div className={this.onlineOffline()}></div>
